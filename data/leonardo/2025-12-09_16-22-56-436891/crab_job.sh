4d4b53ae5bd7179b2aba48a96f0aa64365017de9
lor3ny <lorenzo.piarulli3000@gmail.com>
2025-12-09 16:50:45 +0100

    started bursty analysis

---
+++ data/leonardo/2025-12-09_16-22-56-436891/crab_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=crab_All-to-All
+#SBATCH --output=./data/leonardo/2025-12-09_16-22-56-436891/slurm_output.log
+#SBATCH --error=./data/leonardo/2025-12-09_16-22-56-436891/slurm_error.log
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --dependency=afterany:27896972
+#SBATCH --partition=boost_usr_prod
+#SBATCH --account=IscrB_SWING
+#SBATCH --gres=tmpfs:0
+#SBATCH --time=01:00:00
+
+source /leonardo/home/userexternal/lpiarull/CRAB/.venv/bin/activate
+
+/leonardo/home/userexternal/lpiarull/CRAB/.venv/bin/python /leonardo/home/userexternal/lpiarull/CRAB/cli.py --worker --workdir ./data/leonardo/2025-12-09_16-22-56-436891
