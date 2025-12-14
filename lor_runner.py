8383f3a23e2f378b0a8577c379d2e52971e4e625
lor3ny <lorenzo.piarulli3000@gmail.com>
2025-12-14 18:24:51 +0100

    finita automatizzazione? se spera

--- lor_runner.py
+++ lor_runner.py
@@ -121,7 +121,7 @@ def main():
     BENCHES = ["a2a", "a2a_a2a-cong", "a2a_inc-cong", "agtr", "agtr_a2a-cong", "agtr_inc-cong"]
     pauses = ["0.01","0.0001","0.000001"]
     lengths = ["0.1","0.01","0.001"]
-    node_list = [10, 20, 40, 60] # 80, 160, 250]
+    node_list = [10] #20, 40, 60] 80, 160, 250]
     # system_data = {
     #     "name": "cresco8",
     #     "partition": "cresco8_cpu",
