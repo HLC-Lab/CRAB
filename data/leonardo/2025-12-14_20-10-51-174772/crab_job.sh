6710d7111ffe628ad56a1c061169fabcfb08d606
lor3ny <lorenzo.piarulli3000@gmail.com>
2025-12-15 01:11:48 +0100

    results leo

---
+++ data/leonardo/2025-12-14_20-10-51-174772/crab_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=crab_All-Gather
+#SBATCH --output=./data/leonardo/2025-12-14_20-10-51-174772/slurm_output.log
+#SBATCH --error=./data/leonardo/2025-12-14_20-10-51-174772/slurm_error.log
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --dependency=afterany:29030425
+#SBATCH --partition=boost_usr_prod
+#SBATCH --account=IscrB_SWING
+#SBATCH --time=01:00:00
+
+source /leonardo/home/userexternal/lpiarull/CRAB/.venv/bin/activate
+
+/leonardo/home/userexternal/lpiarull/CRAB/.venv/bin/python /leonardo/home/userexternal/lpiarull/CRAB/cli.py --worker --workdir ./data/leonardo/2025-12-14_20-10-51-174772
