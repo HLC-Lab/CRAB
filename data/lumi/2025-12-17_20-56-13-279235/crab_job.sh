05a91c7e7b878153693a60095eee8c6d6ad417bb
DaXor-0 <saverio.pasqualoni@yahoo.it>
2025-12-17 21:01:23 +0200

    data on lumi started

---
+++ data/lumi/2025-12-17_20-56-13-279235/crab_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=crab_All-Gather
+#SBATCH --output=./data/lumi/2025-12-17_20-56-13-279235/slurm_output.log
+#SBATCH --error=./data/lumi/2025-12-17_20-56-13-279235/slurm_error.log
+#SBATCH --nodes=256
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --dependency=afterany:15413380
+#SBATCH --partition=standard-g
+#SBATCH --account=project_465001736
+#SBATCH --time=01:00:00
+
+source /pfs/lustrep4/users/pasqualo/CRAB/.venv/bin/activate
+
+/pfs/lustrep4/users/pasqualo/CRAB/.venv/bin/python /pfs/lustrep4/users/pasqualo/CRAB/cli.py --worker --workdir ./data/lumi/2025-12-17_20-56-13-279235
