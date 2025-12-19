c5bf82fbd574ed7f51b0b8d731ed610aa06292e9
lor3ny <lorenzo.piarulli3000@gmail.com>
2025-12-19 15:32:08 +0100

    starting fixies

--- huawei_jsons/fast_converter.py
+++ huawei_jsons/fast_converter.py
@@ -6,11 +6,17 @@ if __name__ == "__main__":
     BENCHES = ["a2a", "a2a_a2a-cong", "a2a_inc-cong", "agtr", "agtr_a2a-cong", "agtr_inc-cong"]
     pauses = ["0.01","0.0001","0.000001"]
     lengths = ["0.1","0.01","0.001"]
+    # system_data = {
+    #     "name": "cresco8",
+    #     "partition": "cresco8_cpu",
+    #     "account": "ssheneaadm",
+    #     "path": "/afs/enea.it/fra/user/faltelli/CRAB/wrappers/"
+    # }
     system_data = {
-        "name": "cresco8",
-        "partition": "cresco8_cpu",
-        "account": "ssheneaadm",
-        "path": "/afs/enea.it/fra/user/faltelli/CRAB/wrappers/"
+        "name": "leonardo",
+        "partition": "boost_usr_prod",
+        "account": "IscrB_SWING",
+        "path": "/leonardo/home/userexternal/lpiarull/CRAB/wrappers/"
     }
 
     for bench in BENCHES:
