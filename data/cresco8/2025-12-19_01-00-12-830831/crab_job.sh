c4b6f8c73f61b185458fc888266a657e4f58d528
Marco Faltelli <marcofaltelli@gmail.com>
2025-12-19 01:14:50 +0100

    256 128 ... CRESCO8

---
+++ data/cresco8/2025-12-19_01-00-12-830831/crab_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=crab_All-to-All
+#SBATCH --output=./data/cresco8/2025-12-19_01-00-12-830831/slurm_output.log
+#SBATCH --error=./data/cresco8/2025-12-19_01-00-12-830831/slurm_error.log
+#SBATCH --nodes=256
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+#SBATCH --dependency=afterany:1956737
+#SBATCH --partition=cresco8_cpu
+#SBATCH --account=ssheneaadm
+#SBATCH --qos=ssheneaadm
+#SBATCH --time=01:00:00
+
+source /afs/enea.it/fra/user/faltelli/CRAB/.venv/bin/activate
+
+/afs/enea.it/fra/user/faltelli/CRAB/.venv/bin/python /afs/enea.it/fra/user/faltelli/CRAB/cli.py --worker --workdir ./data/cresco8/2025-12-19_01-00-12-830831
