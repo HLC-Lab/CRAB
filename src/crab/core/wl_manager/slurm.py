342390d7cd4291216c32162c02b5b7bc35bfa604
SharkGamerZ <marcellettimatteo02@gmail.com>
2026-04-29 13:35:51 +0200

    Removed legacy script generator

--- src/crab/core/wl_manager/slurm.py
+++ src/crab/core/wl_manager/slurm.py
@@ -1,19 +1,7 @@
 import os
-import shlex
 
 from typing import List, Optional # Aggiunto typing per chiarezza
 class wl_manager:
-    # Generates a script that can be used to run all the benchmarks specified in the schedule.
-    def write_script(self, runner_args, schedules, nams, name, splits, node_file, ppn):
-        script=open(name,'w+')
-        script.write('#!/bin/bash\nfor schedule in '+' '.join(schedules)+'\ndo\n')
-        script.write('\tfor nam in '+' '.join(nams)+'\n\tdo\n')
-        script.write('\t\tfor split in '+' '.join(splits)+'\n\t\tdo\n')
-        script.write('\t\tpython3 runner.py "$schedule" '+node_file+' -am "$nam" -as "$split"'+runner_args+' -p '+str(ppn))
-        script.write('\n\t\tdone\n\tdone\ndone')
-        script.close()
-
-
     # Returns a string that can be used to run command 'cmd'
     # on the nodes in 'node_list' with 'ppn' processes per node,
     # executing "pre_commands" before cmd
